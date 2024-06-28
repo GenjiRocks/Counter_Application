48f161c1835dbe6c1d43884b11a5c08db8a85134
Ashwin <ashwindeepak1997@gmail.com>
2024-06-28 10:56:46 +0530

    June28

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,25 @@ import { useState } from 'react'
 import './App.css'
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import {useDispatch, useSelector} from 'react-redux'
+import { increment,decrement,reset } from './redux/counterslice';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useSelector((state)=>state.counterReducer.value)
+
+  // To call the actions from couterslice.js
+  const dispatch = useDispatch()
 
   return (
     <>
      <div className='d-flex justify-content-center align-items-center' style={{width:'100%', height:'100vh'}}>
         <div className='bg-light text-center p-4 border rounded-2' style={{width:'450px'}}> 
         <h2>Counter Application</h2>
-        <h1 style={{fontSize:'50px'}}>0</h1>
+        <h1 style={{fontSize:'50px'}}>{count}</h1>
         <div className='mt-5 d-flex justify-content-evenly'>
-        <Button  variant="contained" color='warning'>Decrement</Button>
-        <Button variant="contained" color='error'>Reset</Button>
-        <Button variant="contained" color='success'>Increment</Button>
+        <Button  variant="contained" color='warning' onClick={()=>dispatch(decrement())}>Decrement</Button>
+        <Button variant="contained" color='error' onClick={()=>dispatch(reset())}>Reset</Button>
+        <Button variant="contained" color='success' onClick={()=>dispatch(increment())}>Increment</Button>
         </div>
         <div className='d-flex justify-content-center   mt-5'> 
         <TextField id="outlined-basic" label="Range" variant="outlined" />
---
+++ src/redux/counterslice.js
@@ -0,0 +1,29 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    // put the intial value of the state
+    initialState: {
+        value: 0,
+        },
+        // actions
+        reducers: {
+            increment: (state) => {
+                // logic
+                state.value += 1
+                },
+            decrement:(state)=>{
+                state.value -= 1
+            },
+            reset:(state)=>{
+                state.value = 0
+            }
+        }
+})
+
+// actions are for the components
+export const {increment, decrement, reset} = counterSlice.actions
+
+// reducer is for store
+export default counterSlice.reducer
+
